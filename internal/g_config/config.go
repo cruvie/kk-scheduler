491ad71088564fe62528b8a3599842b1ca9c6cb5
cruvie <@qq>
2026-03-30 15:37:16 +0800

    store

--- internal/g_config/config.go
+++ internal/g_config/config.go
@@ -1,12 +1,16 @@
 package g_config
 
 import (
+	"context"
 	"log/slog"
 	"os"
 
 	"gitee.com/cruvie/kk_go_kit/kk_env"
 	"gitee.com/cruvie/kk_go_kit/kk_pg"
+	"gitee.com/cruvie/kk_go_kit/kk_stage"
+	"gitee.com/cruvie/kk_go_kit/kk_time"
 	"github.com/BurntSushi/toml"
+	"github.com/cruvie/kk-scheduler/internal/models/query"
 )
 
 func init() {
@@ -16,13 +20,14 @@ func init() {
 var Config config
 
 type config struct {
-	HttpPort int
-	GrpcPort int
-	WebPort  int
-	StorePG  *kk_pg.ConfigPG
+	HttpPort   int
+	GrpcPort   int
+	WebPort    int
+	StorePG    *kk_pg.ConfigPG
+	configSlog *kk_stage.ConfigLog `toml:"-"`
 }
 
-func InitConfig() {
+func InitConfig() *kk_stage.Stage {
 	data, err := os.ReadFile("/Users/cruvie/Documents/cruvie/kk-scheduler/config.toml")
 	if err != nil {
 		slog.Error("unable to read config.toml", "err", err)
@@ -34,4 +39,24 @@ func InitConfig() {
 		slog.Error("unable to decode config.toml", "err", err)
 		panic(err)
 	}
+
+	stage := kk_stage.NewStage(context.Background(), "kk-scheduler").SetStartTime(kk_time.NowUTCTime())
+	{
+		Config.configSlog = &kk_stage.ConfigLog{
+			StartTime:  stage.StartTime,
+			Lumberjack: kk_stage.DefaultLogConfig(kk_time.NowUTCTime(), "kk-scheduler"),
+			Format:     kk_stage.FormatJSON,
+		}
+		Config.configSlog.Init()
+	}
+	{
+		Config.StorePG.Init(stage)
+		query.SetDefault(kk_pg.GormClient)
+	}
+	return stage
+}
+
+func CloseConfig() {
+	Config.StorePG.Close()
+	Config.configSlog.Close()
 }
